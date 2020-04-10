47cd212c845649040b32ecfb047cf64ffaf6f293
victorwg <61912133+victorwg@users.noreply.github.com>
2020-04-10 22:20:50 +0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -3,9 +3,16 @@
 
 using namespace std;
 
-string map1[
+string map1[10][10];
+string map2[10][10];
+
+void chooseGameMode() {
+    cout << "Welcome to Battleship!" << endl;
+    cout << "Please choose a game mode: " << endl;
+    
 
 
 int main() {
+    chooseGameMode();
     return 0;
 }
