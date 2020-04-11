4b931fcfeb9ac2cb65d1c1973f67badb9e9423ef
fatfatlam <53366610+fatfatlam@users.noreply.github.com>
2020-04-11 19:04:15 +0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,37 +1,42 @@
 #include <iostream>
 #include <string>
 #include <isstream>
-
 using namespace std;
 
-string map1[10][10];
-string map2[10][10];
-
 void printBoard(string board[]) {
-    
+
 }
 
 void placeship(){
-    
+
 }
 
 void play() {
-    
+
 }
 
 void end() {
-    
+
 }
 
 void num_moves(){
-    
+
 }
 
 int main() {
     //choose game mode
+    int mapx = 10; // map width
+    int mapy = 10; // map length
+    string **map = new string*[mapx]; // create a 2d dynamic array
+    for(int i = 0; i < mapx; ++i) {
+    map[i] = new string[mapy];
+    }
     cout << "Welcome to Battleship!" << endl;
     cout << "Please choose a game mode: " << endl;
-    
-    
+
+    for(int i = 0; i < mapx; ++i) { // delete the 2d dynamic array
+    delete [] map[i];
+    }
+    delete [] map;
     return 0;
 }
